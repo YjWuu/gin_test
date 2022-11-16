71da37a44efc47e4b96a96ad52673972743abc19
YjWuu <99467211+YjWuu@users.noreply.github.com>
2022-11-16 19:35:24 +0800

    Update define.go

--- define/define.go
+++ define/define.go
@@ -6,8 +6,8 @@ var (
 )
 
 const (
-	AccessKey = "20xexftuPIXXLuX0K5bwVX9cKtz5DqmBDAbV5Yt8"
-	SecretKey = "wEQluEzyfODMMEchKrBjD73gwMpFyc2b2tQb61tZ"
-	Bucket    = "xiaocaicai"
-	ImgUrl    = "http://rao4os4ip.hn-bkt.clouddn.com"
+	AccessKey = ""
+	SecretKey = ""
+	Bucket    = ""
+	ImgUrl    = ""
 )
